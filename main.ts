4218bfbc1cea755e500655d9745130b9337caad4
ruheni <aleruheni@outlook.com>
2025-11-30 14:50:48 +0100

    feat: add CORS and refactor CSRF origin handling

--- main.ts
+++ main.ts
@@ -1,13 +1,21 @@
-import { App, csrf, staticFiles } from "fresh";
+import { App, cors, csrf, staticFiles } from "fresh";
 import { type State } from "./utils.ts";
 
 export const app = new App<State>();
 
 app.use(staticFiles());
 
-app.use(csrf({
-  origin: ["http://localhost:8000", "http://localhost:5173"],
-}));
+const ORIGINS = [
+  "http://localhost:8000",
+  "http://localhost:5173",
+];
+app.use(
+  csrf({
+    origin: ORIGINS,
+  }),
+);
+
+app.use(cors({ origin: ORIGINS }));
 
 app.onError("*", (ctx) => {
   console.log(`An error occurred: ${ctx.error}`);
